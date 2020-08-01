33c414e222cd9cc1103520551340ad7454869656
Kyle Rubenok <kyle@rubenok.ca>
2020-08-01 11:59:13 -0700

    Seperate Layout.js into distinct components

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,14 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import catAndHumanIllustration from "../images/cat-and-human-illustration.svg";
 
 function IndexPage() {
   return (
     <Layout>
-    
       <SEO
         title="GatsbyJS Tailwind Starter"
         keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
       />
-
     </Layout>
   );
 }
