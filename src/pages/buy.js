1e3cb71e7115743eb988ae9aa8ccc1e90b50bfdb
Kyle Rubenok <kyle@rubenok.ca>
2020-08-10 22:03:35 -0700

    typeform buy button

--- src/pages/buy.js
+++ src/pages/buy.js
@@ -1,6 +1,4 @@
 import React from "react";
-import Header from "../components/header";
-import Footer from "../components/footer";
 import SEO from "../components/seo";
 import Checkout from "../components/checkout";
 
@@ -12,23 +10,7 @@ function CanceledPage() {
         keywords={[`McGill`, `Management`, `Frosh`, `2020`, `Penguins`]}
       />
 
-      <Header />
-
-      <section className="bg-white py-8">
-        <div className="container max-w-5xl mx-auto m-8">
-          <h2 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
-            Payment
-          </h2>
-          <div className="w-full mb-4">
-            <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
-          </div>
-          <br />
-          <div className="flex justify-center items-center">
-            <Checkout />
-          </div>
-        </div>
-      </section>
-      <Footer />
+      <Checkout url="https://froshofthepenguins.typeform.com/to/yImqrdr9" />
     </body>
   );
 }
