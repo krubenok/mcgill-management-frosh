3db606f7f5526ae0a1974efada76da2aba6c372c
Kyle Rubenok <kyle@rubenok.ca>
2020-08-24 09:14:32 -0700

    add schedule

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -1,37 +1,43 @@
 import React from "react";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import { StaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
 import SEO from "../components/seo";
 
-function SchedulePage() {
-  return (
-    <body className="leading-normal tracking-normal text-white gradient">
-      <SEO
-        title="Schedule"
-        keywords={[`McGill`, `Management`, `Frosh`, `2020`, `Penguins`]}
-      />
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        schedule: file(relativePath: { eq: "schedule.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 1000) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `}
+    render={(data) => (
+      <body className="leading-normal tracking-normal text-white gradient">
+        <SEO
+          title="Schedule"
+          keywords={[`McGill`, `Management`, `Frosh`, `2020`, `Penguins`]}
+        />
 
-      <Header />
+        <Header />
 
-      <section className="bg-white py-8">
-        <div className="container max-w-5xl mx-auto m-8">
-          <h2 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
-            Schedule
-          </h2>
-          <div className="w-full mb-4">
-            <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+        <section className="bg-white py-8">
+          <div className="container max-w-5xl mx-auto m-8">
+            <Img
+              fluid={data.schedule.childImageSharp.fluid}
+              alt="Frosh Schedule"
+            />
           </div>
-          <br />
-          <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
-            Stay tuned for updates regarding our schedule for Management Frosh
-            2020!
-          </h3>
-        </div>
-      </section>
+        </section>
 
-      <Footer />
-    </body>
-  );
-}
-
-export default SchedulePage;
+        <Footer />
+      </body>
+    )}
+  />
+);
